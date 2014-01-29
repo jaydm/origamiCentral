02bff9d7664bce9b4d2e6a1b03e456830270c610
Jay D. McHugh <jaydmchugh@gmail.com>
2014-01-28 20:25:46 -0600

    Progressively building up the home page...

--- index.php
+++ index.php
@@ -12,11 +12,30 @@
     <script src="js/index.js"></script>
   </head>
   <body>
-    <p>Welcome to the new Origami Central!</p>
-    <p>This site has always tried to be the cleanest possible interface
-    to get you the information that you need in order to find origami
-    diagrams either in publications or on the web.</p>
-    <p>For a very long time, the database had no updates (either to its
-    appearance or its contents) that is changing now!</p>
+      <h1>Welcome to OrigamiCentral - The Origami Model Diagram Database</h1>
+      <p>Here you will find easy to use searches to help you in your search for origami model diagrams in print and online.  Hopefully, you will find this utility helpful and easy to use.  If you have any suggestions on how this might be improved you can contact me on Google+ at: https://www.google.com/+JayMcHugh</p>
+      <p>If you have already used the database or would like to just jump in, <a href="#jumpin">Click here to start</a> or keep reading.</p>
+      <p>Even though there are several sites on the web that help you to search for diagrams - we are hoping to make this the easiest to use and most complete resource for finding them.  If you own a book or website that is not already in the database, let me know and I will get it added to the database.  For developers, there is also an API to allow you to connect to the database on your web pages (as long as you credit OrigamiCentral as powering your site).</p>
+      <p>Basic access of the database is fairly straightforward.  Just use the search forms below to search with as little or as much specificity as you like.</p>
+      <form>
+          <div class="row">
+              <div class="small-12 columns">
+                  <label>Model Name</label>
+                  <input type="text" id="modelName" />
+              </div>
+          </div>
+          <div class="row">
+                <div class="small-12 columns">
+                    <label>Designer Name</label>
+                    <input type="text" id="designerName" />
+              </div>
+          </div>
+          <div class="row">
+              <div class="small-12 columns">
+                  <label>Source Publication</label>
+                  <input type="text" id="source" />
+              </div>
+          </div>
+      </form>
   </body>
 </html>
